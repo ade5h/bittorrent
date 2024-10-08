49daf62af23f5aee76f0c8e5f93e20495d762d7d
Adesh Attavar <adesh.attavar@gmail.com>
2024-10-08 18:18:12 +0530

    Removed lombok

--- src/main/java/decoder/DecoderVO.java
+++ src/main/java/decoder/DecoderVO.java
@@ -1,11 +1,19 @@
 package decoder;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class DecoderVO {
     private Object decodedObj;
     private int nextIndex;
+
+    public DecoderVO(Object decodedObj, int nextIndex) {
+        this.decodedObj = decodedObj;
+        this.nextIndex = nextIndex;
+    }
+
+    public Object getDecodedObj() {
+        return decodedObj;
+    }
+
+    public int getNextIndex() {
+        return nextIndex;
+    }
 }
