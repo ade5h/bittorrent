ba8fabf94d5fdc2cf7006f5558ae43285a688f73
Adesh Attavar <adesh.attavar@gmail.com>
2024-10-10 19:31:49 +0530

    Extracting piece hashes

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -2,15 +2,21 @@ import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Torrent {
     private String trackerUrl;
     private long length;
     private String infoHash;
+
+    private long pieceLength;
+    private List<String> pieceHashes;
 
     public Torrent(byte[] torrentMetaInfoBytes) throws NoSuchAlgorithmException {
         Bencode bencode = new Bencode(true);
@@ -21,17 +27,28 @@ public class Torrent {
         byte[] bencodedByteArray = bencode.encode(infoDict);
 
         this.infoHash = hashTheByteArray(bencodedByteArray);
-        this.trackerUrl = StandardCharsets.UTF_8.decode((ByteBuffer) metaInfo.get("announce")).toString();
+        this.trackerUrl = byteBufferToString((ByteBuffer) metaInfo.get("announce"));
         this.length = (long) infoDict.get("length");
+
+        this.pieceLength = (long) infoDict.get("piece length");
+        this.pieceHashes =  getPieceHashesFromPiecesByteBuffer((ByteBuffer) infoDict.get("pieces"));
     }
 
     @Override
     public String toString() {
-        return "Tracker URL: " + this.trackerUrl +
-                System.lineSeparator() +
-                "Length: " + this.length +
-                System.lineSeparator() +
-                "Info Hash: " + this.infoHash;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Tracker URL: ").append(this.trackerUrl).append(System.lineSeparator())
+                .append("Length: ").append(this.length).append(System.lineSeparator())
+                .append("Info Hash: ").append(this.infoHash).append(System.lineSeparator())
+                .append("Piece Length: ").append(this.pieceLength).append(System.lineSeparator())
+                .append("Piece Hashes:").append(System.lineSeparator());
+
+        for (String pieceHash: pieceHashes) {
+            stringBuilder.append(pieceHash).append(System.lineSeparator());
+        }
+
+        return stringBuilder.toString();
     }
 
     private String hashTheByteArray(byte[] bytes) throws NoSuchAlgorithmException {
@@ -39,9 +56,39 @@ public class Torrent {
         md.update(bytes);
         byte[] digest = md.digest();
 
+        return byteArrayToHexaDecimal(digest);
+    }
+
+    private String byteBufferToString(ByteBuffer byteBuffer) {
+        return StandardCharsets.UTF_8.decode(byteBuffer).toString();
+    }
+
+    private List<String> getPieceHashesFromPiecesByteBuffer(ByteBuffer pieces) throws NoSuchAlgorithmException {
+        byte[] byteArrayOfPieceHashes = new byte[pieces.remaining()];
+        pieces.get(byteArrayOfPieceHashes);
+
+        List<String> pieceHashes = new ArrayList<>();
+
+        int currentSize = 0;
+        byte[] currentHash = new byte[20];
+
+        for (int index = 0; index < byteArrayOfPieceHashes.length; index++) {
+            currentHash[index % 20] = byteArrayOfPieceHashes[index];
+            currentSize++;
+
+            if(currentSize == 20) {
+                pieceHashes.add(byteArrayToHexaDecimal(currentHash));
+                currentSize = 0;
+            }
+        }
+
+        return pieceHashes;
+    }
+
+    private String byteArrayToHexaDecimal(byte[] bytes) {
         StringBuilder hexString = new StringBuilder();
 
-        for (byte b : digest) {
+        for (byte b : bytes) {
             hexString.append(String.format("%02x", b));
         }
 
