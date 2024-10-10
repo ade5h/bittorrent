86df2d21388d9eb8419a66d9ff3cd524c3805224
Adesh Attavar <adesh.attavar@gmail.com>
2024-10-10 14:50:48 +0530

    Calculating the info hash of the torrent metainfo

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -1,24 +1,48 @@
 import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 public class Torrent {
     private String trackerUrl;
     private long length;
+    private String infoHash;
 
-    public Torrent(byte[] torrentMetaInfoBytes) {
-        Bencode bencode = new Bencode();
+    public Torrent(byte[] torrentMetaInfoBytes) throws NoSuchAlgorithmException {
+        Bencode bencode = new Bencode(true);
 
         Map<String, Object> metaInfo = bencode.decode(torrentMetaInfoBytes, Type.DICTIONARY);
         Map<String, Object> infoDict = (Map<String, Object>) metaInfo.get("info");
 
-        this.trackerUrl = (String) metaInfo.get("announce");
+        byte[] bencodedByteArray = bencode.encode(infoDict);
+
+        this.infoHash = hashTheByteArray(bencodedByteArray);
+        this.trackerUrl = metaInfo.get("announce").toString();
         this.length = (long) infoDict.get("length");
     }
 
     @Override
     public String toString() {
-        return "Tracker URL: " + this.trackerUrl + System.lineSeparator() + "Length: " + this.length;
+        return "Tracker URL: " + this.trackerUrl +
+                System.lineSeparator() +
+                "Length: " + this.length +
+                System.lineSeparator() +
+                "Info Hash: " + this.infoHash;
+    }
+
+    private String hashTheByteArray(byte[] bytes) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(bytes);
+        byte[] digest = md.digest();
+
+        StringBuilder hexString = new StringBuilder();
+
+        for (byte b : digest) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        return hexString.toString();
     }
 }
