d5d70aab80c025ad035f87aac6ab1353d26bcbbb
Adesh Attavar <adesh.attavar@gmail.com>
2024-10-10 13:13:48 +0530

    Reading url and length from torrent metainfo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
             }
             System.out.println(gson.toJson(decoded));
         }
-        if("info".equals(command)) {
+        else if("info".equals(command)) {
             String torrentFileName = args[1];
 
             File torrentFile = new File(torrentFileName);
